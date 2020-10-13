2d01b852fe4230a1c22a12e7baa624d900978038
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-10-13 14:26:20 -0600

    got recipe form working and began styling

--- src/components/lunchLine/recipeForm.js
+++ src/components/lunchLine/recipeForm.js
@@ -1,42 +1,48 @@
 import React, { useState, useEffect } from 'react';
 
 import { connect } from 'react-redux';
-import { addRecipe, updateRecipeName, updateActiveItem } from '../../store/lunchLine.js';
+import { addRecipe } from '../../store/lunchLine.js';
 
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
-
-
-function RecipeForm ({ addRecipe, displayRecipes, activeItem, updateRecipeName, updateActiveItem}) {
-  const [post, setPost] = useState([{}]);
+function RecipeForm ({ addRecipe }) {
   
-  console.log('display reicpes', displayRecipes)
-  console.log('recipe name', activeItem)
+  const [name, setName] = useState('');
+  const [prepTime, setPrepTime] = useState(0);
+  const [ingredients, setIngredients]= useState([]);
+  const [directions, setDirections] = useState([]);
+
+  const classes = useStyles();
+
+ console.log('recipe', ([name, prepTime, ingredients, directions]));
   return (
     <div>
       <h2>Add a recipe!</h2> 
-      <form onSubmit={e => {
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={e => {
         e.preventDefault();
-        // addRecipe(recipes.recipeName);
-        addRecipe();
-        setPost('');
+        addRecipe(name, prepTime, ingredients, directions);
       }}>
-        {/* {console.log('display reicpes', displayRecipes)}; */}
-        <input type='text' name='recipeName' placeholder='Recipe name' onChange={(event) => updateActiveItem({ "recipeName": event.target.value })}></input>
+        <input type='text' name='recipeName' placeholder='Recipe name' value={name} 
+        onChange={(event) => setName(event.target.value)}></input>
         <br/>
-        <input type='number' name='prepTime' value={displayRecipes.prepTime} placeholder='Prep time' onChange={(event) => updateActiveItem({ "prepTime": event.target.value })} ></input>
+        <input type='number' name='prepTime' value={prepTime} placeholder='Prep time' onChange={(event) => setPrepTime(event.target.value)} ></input>
         <br/>
-        <input type='text' name='ingredients' value={displayRecipes.ingredients} placeholder='Ingredients' onChange={(event) => updateActiveItem({ "ingredients": event.target.value })} ></input>
+        <input type='text' name='ingredients' value={ingredients} placeholder='Ingredients' onChange={(event) => setIngredients(event.target.value)} ></input>
         <br/>
-        <input type='textarea' name='directions' value={displayRecipes.directions} placeholder='Directions' onChange={(event) => updateActiveItem({ "directions": event.target.value })} ></input>
+        <input type='textarea' name='directions' value={directions} placeholder='Directions' onChange={(event) => setDirections(event.target.value)} ></input>
         <br/>
       <button type="submit">Post your Recipe!</button>
       </form>
-{/* recipe name
-    image
-    prep time
-    ingredients
-    directions */}
     </div>
   )
 } 
@@ -49,5 +55,5 @@ const mapStateToProps = state => {
   }
 }
 
-const mapDispatchToProps = { addRecipe, updateRecipeName, updateActiveItem}
+const mapDispatchToProps = { addRecipe }
 export default connect(mapStateToProps, mapDispatchToProps)(RecipeForm);
