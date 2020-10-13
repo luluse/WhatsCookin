eda9f5bad8239b3758cb6f2cfa2260d9dc80a1d1
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-10-13 16:30:25 -0700

    Added constant file to easily swap out API URL

--- src/store/lunchLine.js
+++ src/store/lunchLine.js
@@ -1,4 +1,6 @@
-const axios = require('axios');
+import API from '../constants/url.js';
+import axios from 'axios';
+
 /*
 recipe name string
 thumbnail url string
@@ -8,65 +10,65 @@ directions array of strings
 */
 
 let initialState = {
-  recipes: [
-    { recipeName: 'Apple Pie', thumbnailUrl: '', prepTime: 40, ingredients: ['apple', 'pie crust', 'whipped cream'], directions: ['bake pie'] },
-    { recipeName: 'Corn Bread', thumbnailUrl: '', prepTime: 35, ingredients: ['corn', 'flour', 'eggs'], directions: ['bake in the oven'] },
-  ],
+    recipes: [
+        { recipeName: 'Apple Pie', thumbnailUrl: '', prepTime: 40, ingredients: ['apple', 'pie crust', 'whipped cream'], directions: ['bake pie'] },
+        { recipeName: 'Corn Bread', thumbnailUrl: '', prepTime: 35, ingredients: ['corn', 'flour', 'eggs'], directions: ['bake in the oven'] },
+    ],
 };
 
 
 export default (state = initialState, action) => {
-  let { type, payload } = action;
-  console.log('recipeArray', state.recipes);
+    let { type, payload } = action;
+    console.log('recipeArray', state.recipes);
 
-  switch (type) {
-
-
-    // Add recipe post from the form
-    case 'ADD_POST':
-
-      // return{ ...state, recipes: [...state.recipes, payload]};
-      return { ...state, recipes: [...state.recipes, payload] };
+    switch (type) {
 
 
-    // get recipes we have stored
-    case 'GET_POSTS':
-      return { ...state, recipes: payload };
+        // Add recipe post from the form
+        case 'ADD_POST':
+
+            // return{ ...state, recipes: [...state.recipes, payload]};
+            return { ...state, recipes: [...state.recipes, payload] };
 
 
-    default:
-      return state;
-  }
+        // get recipes we have stored
+        case 'GET_POSTS':
+            return { ...state, recipes: payload };
+
+
+        default:
+            return state;
+    }
 };
 
 
 export const addRecipe = (recipeName, prepTime, ingredients, directions) => {
 
-  return {
-    type: 'ADD_POST',
-    payload: {
-      recipeName,
-      prepTime,
-      ingredients,
-      directions,
-    },
-  };
+    return {
+        type: 'ADD_POST',
+        payload: {
+            recipeName,
+            prepTime,
+            ingredients,
+            directions,
+        },
+    };
 };
 
 export function getRecipes() {
 
-  return async function (dispatch) {
+    return async function (dispatch) {
 
-    const response = await axios.get('http://localhost:3009/api/recipe');
+        const response = await axios.get(API.BASE + API.RECIPE);
 
-    console.log('response data', response.data);
+        console.log('response data', response.data);
 
-    dispatch({
-      type: 'GET_POSTS',
-      payload: response.data,
-    });
+        dispatch({
+            type: 'GET_POSTS',
+            payload: response.data,
+        });
 
-  };
+    };
 
 };
 
