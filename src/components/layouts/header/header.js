9c1b7e578ec8ab94cd9d5db6dc2c015f1dc1e246
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-10-13 16:45:28 -0700

    fixed error with attribute

--- src/components/layouts/header/header.js
+++ src/components/layouts/header/header.js
@@ -9,32 +9,32 @@ import { makeStyles } from '@material-ui/core/styles';
 
 
 function Header() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      color: theme.palette.warning.main,
-    },
-  }));
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            color: theme.palette.warning.main,
+        },
+    }));
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <header>
-      <img
-        src={picture}
-        width="40"
-        height="40"
-        className="d-inline-block align-top"
-        alt="Chef Logo"
-      />
-      <h1>What's cookin</h1>
-      <br />
-      <Link to='/'><HomeIcon backgroundColor="orange[500]"/></Link>
-      
-      <Link to='/profile/'><AccountCircleIcon/></Link>
-  
-      <Link to='/search/'><SearchIcon/></Link>
-    </header>
-  )
+    return (
+        <header>
+            <img
+                src={picture}
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+                alt="Chef Logo"
+            />
+            <h1>What's cookin</h1>
+            <br />
+            <Link to='/'><HomeIcon backgroundcolor="orange[500]" /></Link>
+
+            <Link to='/profile/'><AccountCircleIcon /></Link>
+
+            <Link to='/search/'><SearchIcon /></Link>
+        </header>
+    )
 }
 
 export default Header;
