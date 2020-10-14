c3588262201aff1d00b58db4f0f6b9e2c4157685
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-10-14 10:56:54 -0700

    Fake login

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import React from 'react';
 // import './App.css';
 import Header from './components/layouts/header/header';
 import LunchLine from './components/lunchLine/LunchLine';
+import Login from './components/auth/login.js';
 import Profile from './components/profile/profile';
 import Search from './components/search/search';
 import Footer from './components/layouts/footer/footer';
 
 import { Route, Switch } from 'react-router-dom';
 
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main>
-      <Switch>
-        <Route path='/' component={LunchLine} exact/>
-        <Route path='/profile' component={Profile} />
-        <Route path='/search' component={Search} />
-        <Route/>
-      </Switch>
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <main>
+                <Switch>
+                    <Route path='/' component={Login} exact />
+                    <Header />
+                    <Route path='/home' component={LunchLine} />
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/search' component={Search} />
+                    <Route />
+                </Switch>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
